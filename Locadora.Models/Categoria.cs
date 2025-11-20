e208a19804955b0bb125989a5ee57334c7ac526d
MatheusPRodrigues <matherodrigues17@gmail.com>
2025-11-20 15:57:46 -0300

    feat: Add models de Veiculo e Categoria

---
+++ Locadora.Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public decimal Diaria { get; private set; }
+    }
+}
---
+++ Locadora.Models/Veiculo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Veiculo
+    {
+        public int VeiculoID { get; private set; }
+        public int CategoriaID { get; private set; }
+        public string Marca { get; private set; }
+        public string Placa { get; private set; }
+        public string Modelo { get; private set; }
+        public int Ano { get; private set; }
+        public string StatusVeiculo { get; private set; }
+
+        public Veiculo(
+            int categoriaID,
+            string marca,
+            string placa, 
+            string modelo, 
+            int ano,
+            string statusVeiculo
+        )
+        {
+            CategoriaID = categoriaID;
+            Marca = marca;
+            Placa = placa;
+            Modelo = modelo;
+            Ano = ano;
+            StatusVeiculo = statusVeiculo;
+        }
+
+        public void SetVeiculoID(int veiculoId)
+        {
+            VeiculoID = veiculoId;
+        }
+
+        public void SetStatusVeiculo(string statusVeiculo )
+        {
+            StatusVeiculo = statusVeiculo;
+        }
+
+        public override string? ToString()
+        {
+            return $"Placa: {Placa}\n" +
+                $"Marca: {Marca}\n" +
+                $"Modelo: {Modelo}\n" +
+                $"Ano: {Ano}\n" +
+                $"Status: {StatusVeiculo}\n";
+        }
+    }
+}
